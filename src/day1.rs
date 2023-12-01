62fbacf5c6ad9df543ac8b891f4ee31ce3d82f42
Kyle Harris <kaharrs2@gmail.com>
2023-11-30 23:18:55 -0700

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,46 +1,78 @@
-use std::collections::{HashMap, HashSet, VecDeque};
-use scan_fmt::scan_fmt;
-use std::io::Write;
-
-type Input = usize;
-
-fn print(board: &Vec<Vec<usize>>, breakpoint: bool) {
-    for x in board {
-        for y in x {
-            print!(".");
+fn check(asdf: &str, dumb: bool) -> Option<i64> {
+    //println!("asdf: {}", asdf);
+    if dumb {
+        if asdf.len() >= 3 && &asdf[..3] == "one" {
+            return Some(1);
+        } else if asdf.len() >= 3 && &asdf[..3] == "two" {
+            return Some(2);
+        } else if asdf.len() >= 5 && &asdf[..5] == "three" {
+            return Some(3);
+        } else if asdf.len() >= 4 && &asdf[..4] == "four" {
+            return Some(4);
+        } else if asdf.len() >= 4 && &asdf[..4] == "five" {
+            return Some(5);
+        } else if asdf.len() >= 3 && &asdf[..3] == "six" {
+            return Some(6);
+        } else if asdf.len() >= 5 && &asdf[..5] == "seven" {
+            return Some(7);
+        } else if asdf.len() >= 5 && &asdf[..5] == "eight" {
+            return Some(8);
+        } else if asdf.len() >= 4 && &asdf[..4] == "nine" {
+            return Some(9);
         }
-        println!();
     }
-    println!();
 
-    if breakpoint {
-        // Get user input to continue to next step
-        std::io::stdout().flush();
-        let mut asdf = String::new();
-        std::io::stdin().read_line(&mut asdf);
+    if asdf.chars().nth(0).unwrap() == '1' {
+        Some(1)
+    } else if asdf.chars().nth(0).unwrap() == '2' {
+        Some(2)
+    } else if asdf.chars().nth(0).unwrap() == '3' {
+        Some(3)
+    } else if asdf.chars().nth(0).unwrap() == '4' {
+        Some(4)
+    } else if asdf.chars().nth(0).unwrap() == '5' {
+        Some(5)
+    } else if asdf.chars().nth(0).unwrap() == '6' {
+        Some(6)
+    } else if asdf.chars().nth(0).unwrap() == '7' {
+        Some(7)
+    } else if asdf.chars().nth(0).unwrap() == '8' {
+        Some(8)
+    } else if asdf.chars().nth(0).unwrap() == '9' {
+        Some(9)
+    } else {
+        None
     }
 }
 
-#[aoc_generator(dayN)]
-pub fn load_input(input: &str) -> Vec<Input> {
+#[aoc_generator(day1)]
+pub fn load_input(input: &str) -> String {
+    input.to_string()
+}
+
+fn parse(input: &str, dumb: bool) -> Vec<i64> {
     let mut output = vec![];
-    for line in input.lines() {
-        output.push(line.parse::<Input>().unwrap());
-        //output.push(scan_fmt!(line, "{} {}", char, char).unwrap());
+    for _line in input.lines() {
+        let mut digits = vec![];
+        for i in 0.._line.len() {
+            if let Some(value) = check(&_line[i..], dumb) {
+                digits.push(value);
+            }
+        }
+        //println!("{:?}", digits);
+        output.push(10 * digits.first().unwrap() + digits.last().unwrap());
     }
     output
 }
 
-#[aoc(dayN, part1)]
-pub fn part1(input: &[Input]) -> usize {
-
-    0
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> usize {
+    parse(input, false).into_iter().map(|x| x as usize).sum()
 }
 
-#[aoc(dayN, part2)]
-pub fn part2(input: &[Input]) -> usize {
-
-    0
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> usize {
+    parse(input, true).into_iter().map(|x| x as usize).sum()
 }
 
 #[cfg(test)]
@@ -50,15 +82,15 @@ mod test {
 
     #[test]
     fn test_part1() {
-        let input = read_to_string("input/2023/XXa.txt").unwrap();
+        let input = read_to_string("input/2023/01a.txt").unwrap();
         let input = load_input(&input);
-        assert_eq!(part1(&input), 0);
+        assert_eq!(part1(&input), 142);
     }
 
     #[test]
     fn test_part2() {
-        let input = read_to_string("input/2023/XXa.txt").unwrap();
+        let input = read_to_string("input/2023/01b.txt").unwrap();
         let input = load_input(&input);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 281);
     }
 }
